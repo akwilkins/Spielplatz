952da1725d512569cdbea1f6af7e69c1698badaf
akwilkins <akwilkins@appliedbiomath.com>
2015-10-15 11:49:59 -0400

    Update Eis_aussuchen.py
    umlaute

--- Eis_aussuchen.py
+++ Eis_aussuchen.py
@@ -1,7 +1,7 @@
 #-*- coding: cp1252 -*-
 
 import easygui
-eis = easygui.buttonbox("Was wollen Sie fuer eine Eissorte?", choices = ['Himbeer', 'Erdbeer', 'Kirsch', 'Waldfrüchte', 'Heidelbeere', 'Schoko', 'Vanille', 'Mokka', 'Rocher', 'Mango', 'Granatapfel', 'Cranberry', 'Sesam', 'Jne', 'Limmette', 'Karamel mit gesalzener Butter', 'Smarties', 'Casis', 'Orange', 'Mandarine', 'Malaga', 'Schlumpfeis', 'Tutti Frutti', 'Praline', 'Amarena-Kirsch', 'Zitrus-Mix', 'Kafee-eis', 'Papaya', 'Ananas', 'After-Eight', 'Milchschokolade', 'Weisseschokolade', 'Snickerseis', 'Marseis', 'Twixeis', 'Bountyeis'])
+eis = easygui.buttonbox("Was wollen Sie fuer eine Eissorte?", choices = ['Himbeer', 'Erdbeer', 'Kirsch', 'Waldfrucht', 'Heidelbeere', 'Schoko', 'Vanille', 'Mokka', 'Rocher', 'Mango', 'Granatapfel', 'Cranberry', 'Sesam', 'Jne', 'Limmette', 'Karamel mit gesalzener Butter', 'Smarties', 'Casis', 'Orange', 'Mandarine', 'Malaga', 'Schlumpfeis', 'Tutti Frutti', 'Praline', 'Amarena-Kirsch', 'Zitrus-Mix', 'Kafee-eis', 'Papaya', 'Ananas', 'After-Eight', 'Milchschokolade', 'Weisseschokolade', 'Snickerseis', 'Marseis', 'Twixeis', 'Bountyeis'])
 
 
 if eis:
